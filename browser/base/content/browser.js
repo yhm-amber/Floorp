74453699d4bddabd9312cb21efbb7500daeb5539
surapunoyousei <ryosukeusuusu@gmail.com>
2022-05-13 00:38:21 +0900

    add Gnome Theme & Fix Gnome Theme bug

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -10000,7 +10000,6 @@ const PhotonUI = `@import url(chrome://browser/skin/photon/photonChrome.css);
 const MaterialUI = `@import url(chrome://browser/skin/floorplegacy/floorplegacy.css);`
 const fluentUI = `@import url(chrome://browser/skin/fluentUI/fluentUI.css);`
 const gnomeUI = `@import url(chrome://browser/skin/gnomeUI/gnomeUI.css);`
-const WebkitUI = `@import url(chrome://browser/skin/webkitUI/webkitUI.css);`
 
 switch(floorpinterfacenum){
 
@@ -10036,20 +10035,13 @@ switch(floorpinterfacenum){
     document.getElementsByTagName('head')[0].insertAdjacentElement('beforeend',Tag);
     break;
 
-  case 6:
-     if (AppConstants.platform == "linux"){
-     var Tag = document.createElement('style');
-     Tag.innerText = gnomeUI;
-     document.getElementsByTagName('head')[0].insertAdjacentElement('beforeend',Tag);
-     }
-    break;
-
-  case 7:
-     var Tag = document.createElement('style');
-     Tag.innerText = WebkitUI;
-     document.getElementsByTagName('head')[0].insertAdjacentElement('beforeend',Tag);
-  break;
-  
+    case 6:
+      if (AppConstants.platform == "linux"){
+      var Tag = document.createElement('style');
+      Tag.innerText = gnomeUI;
+      document.getElementsByTagName('head')[0].insertAdjacentElement('beforeend',Tag);
+      }
+      break;
 }
 
 
